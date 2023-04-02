1e8829ca3d595c2a246ce12ae4dc6cf8db282456
guilhem13 <guilhem.maille@gmail.com>
2023-04-02 17:13:25 +0200

    first route

---
+++ api/__init__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from flask import Flask
+from api import router
+
+def create_app(test_config=None):
+    """Create and configure the flask app with the factory pattern"""
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_pyfile("config.py", silent=False)
+        app.config.update(test_config)
+
+    # Register the router
+    app.register_blueprint(router.bp)
+
+    return app
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+"""
+Name: Data Reliability Demo
+Author: Guilhem Maillebuau
+Api which outlow the concept of data reliability as code 
+"""
 
-app = Flask(__name__)
+from waitress import serve
+from api import create_app
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":   
+
+    app = create_app()
+    serve(app, host="0.0.0.0", port=5000)
